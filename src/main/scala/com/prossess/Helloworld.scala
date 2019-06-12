76dc58645b299c7b7f89002e045e5cb5ab679f76
Saheed Adepoju <adepoju@gmail.com>
2019-06-12 00:18:51 -0700

    An update of code based for Math library

--- src/main/scala/com/prossess/Helloworld.scala
+++ src/main/scala/com/prossess/Helloworld.scala
@@ -1,10 +1,33 @@
 package com.prossess
 
-object Helloworld extends App  {
+object Helloworld   {
 
-  println("Hello world")
+  def main(args: Array[String]): Unit ={
 
-  var a = 4 + 6;
-  println(a);
+    println("Hello world")
+    //println(add(5,6))
+    //list: List[Int] = List(1, 2, 3)
+    val list = List(1,2,3,7,8,4,11.8)
+    println(mean(list))
+  }
+
+  def add(first: Int, second: Int) : Int ={
+
+     first+second
+  }
+
+  def mean(values: List[Double]): Double ={
+
+     var sum:Double  = 0
+    for(v: Double <- values){
+        sum = sum + v
+
+      }
+      val mean = sum/values.length
+
+     mean
+  }
+
+
 
 }
